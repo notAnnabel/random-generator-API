8416bc0ca0898ec45156bad44ca984242fd2f8a2
notAnnabel <Annabel.Dornan@student.norwichuni.ac.uk>
2024-10-29 12:29:04 +0000

    more edits

--- database.js
+++ database.js
@@ -1,33 +1,66 @@
 
 const breedSelect = document.getElementById("breed-select");
+const randomDogButton = document.getElementById("random-dog-button");
+
 const subBreedSelect = document.getElementById("sub-breed-select");
+const dogImage = document.getElementById("dog-image");
+
+
 
 async function fetchBreedPosibilities() {
     const breedListUrl = "https://dog.ceo/api/breeds/list";
 
-    try {
-        // get response from api
-        const response = await fetch(breedListUrl);
+    const breedList = await fetchFromApi(breedListUrl);
 
+
+    for (const breed of breedList) {
+        const newOption = document.createElement("option");
+        newOption.text = breed;
+        breedSelect.options.add(newOption, breed);
+    }
+}
+
+async function fetchRandomDog() {
+    let randomDogUrl = "https://dog.ceo/api/breeds/image/random";
+    if (breedSelect.value == "any"){
+        
+        const imageSource = await fetchFromApi(randomDogUrl);
+
+        dogImage.src = imageSource;
+        
+        
+    } else {
+        const randomDogUrl = "https://dog.ceo/api/breed/" +breedSelect.value + "/images/random";
+        const imageSource = await fetchFromApi(randomDogUrl);
+
+        dogImage.src = imageSource;
+    }
+}
+
+
+// fetch info from api 
+async function fetchFromApi(url) {
+    try {
+        const response = await fetch(url);
 
         if (!response.ok) {
-            throw new Error("Response Status: " + response.status);
+            throw new Error("reponse status: " + response.status);
         }
 
         const json = await response.json();
 
-        const breedList = json.message;
+        return json.message;
 
-        for (const breed of json){
-            const newOption = document.getElementById("option");
-            newOption.text = breed;
-            breedSelect.options.add(newOption, breed);
-        }
-
-
-    } catch (error) {
-        console.error(error.message)
+    } catch(error) {
+        console.log(error)
     }
+
 }
+// link buttons
+
+randomDogButton.onclick = fetchRandomDog;
+
 
 fetchBreedPosibilities();
+
+fetchRandomDog();
