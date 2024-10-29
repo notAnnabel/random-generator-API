41c85380461638145bf4a38aa2b269521ebec56b
notAnnabel <Annabel.Dornan@student.norwichuni.ac.uk>
2024-10-29 15:46:58 +0000

    i can favourite the images

--- favourites.js
+++ favourites.js
@@ -3,12 +3,36 @@ const favouritesDiv = document.getElementById("favourites-div")
 
 let currentImage = undefined;
 
+let favouritesList = [];
+
+
+
 function onAddFavouriteButtonClick(){
+    if (favouritesList.includes(currentImage)) return;
+    
+    // add currently displayed image to favourite
+    favouritesList.push(currentImage);
+
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("container", "flex-column", "favourite-card")
+
     const newImage = document.createElement("img");
     newImage.src = currentImage;
-    newImage.classList.add("favourite-img")
+    newImage.classList.add("favourite-img");
+    
+    const downloadButton = document.createElement("button");
+    downloadButton.innerHTML = "Download"
 
+    // append newDiv childs
+    newDiv.append(newImage);
+    newDiv.append(downloadButton);
+    
+    //append the child
     favouritesDiv.append(newImage);
+
+    // append div to favourites div
+    favouritesDiv.append(newDiv);
+
 }
 
 addFavouriteButton.onclick = onAddFavouriteButtonClick;
